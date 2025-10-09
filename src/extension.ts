72b4912b3166bdcf4dc337c00262991982a9de8a
Christian Rab <rab@mpe.mpg.de>
2025-10-10 01:43:48 +0200

    Some first experiments with autocompletion for parameter names.

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
 import { DocumentSymbol } from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,6 +21,13 @@ export function activate(context: vscode.ExtensionContext) {
         )
     );
 
+    context.subscriptions.push(
+        vscode.languages.registerCompletionItemProvider(
+            { scheme: 'file', language: 'prodimoparam' },
+            new ParameterNameCompletionProvider(context),
+        '!' // Optional: trigger character(s)
+        )
+    );
 
 	// // The command has been defined in the package.json file
 	// // Now provide the implementation of the command with registerCommand
@@ -32,6 +41,45 @@ export function activate(context: vscode.ExtensionContext) {
 	// context.subscriptions.push(disposable);
 }
 
+class ParameterNameCompletionProvider implements vscode.CompletionItemProvider {
+    private paramListPath: string;
+    constructor(private context: vscode.ExtensionContext) {
+        this.paramListPath = context.asAbsolutePath('paramlist.json');
+    }
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+        // Return an array of CompletionItem objects
+        const completionItems = [];
+
+        const line = document.lineAt(position);
+        //const wordRange = document.getWordRangeAtPosition(position);
+        const paramListRaw = fs.readFileSync(this.paramListPath, 'utf8');
+        const paramList = JSON.parse(paramListRaw);
+
+        for (const param of paramList.parameters) {
+            const item = new vscode.CompletionItem(param.name, vscode.CompletionItemKind.Text);
+
+            let insertText = ' ' + param.name;
+
+            if (line.text.trimEnd().endsWith('!')) {
+                if (param.unit && param.unit !== '-') {
+                    insertText += "   [" + param.unit + "]";
+                }
+                if (param.description) {
+                    insertText += "   : " + param.description;
+                }
+            }
+            //item.detail = param.type;
+            item.documentation = new vscode.MarkdownString(`- **Type:** ${param.type}\n- **Default:** ${param.default}\n- **Unit:** ${param.unit}\n- **Description:** ${param.description}\n`);
+            //item.documentation = new vscode.MarkdownString(`- Description: ${param.description}\n- Type: ${param.type}\n- Unit: ${param.unit}\n- Default: ${param.default}`);
+            item.insertText = insertText;
+            completionItems.push(item);
+        }
+        
+        return completionItems;
+    }
+}
+
+
 class ProDiMoParamDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
     public provideDocumentSymbols(
         document: vscode.TextDocument,
@@ -41,8 +89,7 @@ class ProDiMoParamDocumentSymbolProvider implements vscode.DocumentSymbolProvide
         {
             const symbols: vscode.DocumentSymbol[] = [];
             const nodes = [symbols];
-            vscode.window.showInformationMessage('provideDocumentSymbols called');
-
+            //vscode.window.showInformationMessage('provideDocumentSymbols called');
             
             for (let line = 0; line < document.lineCount; line++) {
                 const textLine = document.lineAt(line);
